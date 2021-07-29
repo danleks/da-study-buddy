546d5e20610520bcd4a0f82cbdb58e412866025e
danleks <danleks@pop-os.localdomain>
2021-07-29 07:56:19 +0200

    create users-list component

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UsersList from 'components/UsersList/UsersList';
 
 const Root = (props) => {
-  return <div>test</div>;
+  return <UsersList />;
 };
 
 Root.propTypes = {};
